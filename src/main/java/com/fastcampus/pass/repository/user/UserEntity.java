ce54d51621dbda292fa8c6c1e291b0e832e47b26
stone <astrataraxia@gmail.com>
2023-02-17 21:32:23 +0900

    query patch

--- src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
+++ src/main/java/com/fastcampus/pass/repository/user/UserEntity.java
@@ -1,17 +1,22 @@
 package com.fastcampus.pass.repository.user;
 
 import com.fastcampus.pass.repository.BaseEntity;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.util.Map;
 
 @Getter
 @Setter
 @ToString
 @Entity
-@Table(name = "user")
+@Table(name = "user" )
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class UserEntity extends BaseEntity {
     @Id
     private String userId;
@@ -20,6 +25,16 @@ public class UserEntity extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
     private String phone;
-    private String meta;
 
+    @Type(type = "json")
+    private Map<String, Object> meta;
+
+    public String getUuid() {
+        String uuid = null;
+
+        if (meta.containsKey("uuid"))
+            uuid = String.valueOf(meta.get("uuid"));
+
+        return uuid;
+    }
 }
